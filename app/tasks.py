67e8d0a027f78b4d1fd75e7302e8afea2cc1f4c1
veski4a <78254623+Veselin15@users.noreply.github.com>
2025-11-25 14:47:09 +0200

    Delete app directory

--- app/tasks.py
+++
@@ -1,7 +0,0 @@
-import time
-from .worker import celery_app
-
-@celery_app.task
-def ai_response_task(name: str):
-    time.sleep(5)  # Simulation
-    return f"Hello, {name}! This is a asynchronous bot answer."
